3dc67f2ccd659f96868bf235b576adc88ab59b20
Ivan Costa <ivan.felipe@payshopx.com.br>
2021-05-09 12:06:04 -0300

    feat: day 49 - todo list;
    Signed-off-by: Ivan Costa <ivan.felipe@payshopx.com.br>

---
+++ day 49/script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUl = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerHTML = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
+
+    todoEl.addEventListener("dblclick", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUl.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
